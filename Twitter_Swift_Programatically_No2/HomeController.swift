2553085da99ca1997372146ca0c12a7467b2b964
masato <masato@m-mbp.local>
2018-10-20 14:16:24 +0800

    Adjust Cell hight & make WordCell class & set WordCell Class to .register -: view the yellow cells

--- Twitter_Swift_Programatically_No2/HomeController.swift
+++ Twitter_Swift_Programatically_No2/HomeController.swift
@@ -8,9 +8,53 @@
 
 import UIKit
 
-class HomeController: UICollectionViewController {
+class WordCell: UICollectionViewCell {
+
+    // this gets called when a cell is dequeued
+    // cellがデキューされたときに呼び出されるように、initを設定する
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .yellow
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+
+
+class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    let cellId = "cellId"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = .green
+        collectionView?.backgroundColor = .white
+        collectionView?.register(WordCell.self, forCellWithReuseIdentifier: cellId)
     }
+
+
+
+    // データの個数を返すメソッド
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+
+    // データを返すメソッド
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+//        cell.backgroundColor = .blue
+        return cell
+    }
+
+    // Adjust Collection View Hight
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+
+
 }
+
